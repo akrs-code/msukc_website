0f42b71e1908f6fa14986dbb90a27317b1fe67cc
Beelzebuboga <abdulkhaliqsolaiman@gmail.com>
2025-04-18 23:16:43 +0800

    created the initial navigation bar and hero for my new karate landing website

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=m, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap" rel="stylesheet">
+    <title>MSUKC</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <?php wp_header()?>
+</head>
+<body>
+    /*Navigation*/
+    <nav class="bg-white text-black flex justify-center md:justify-evenly  h-20 items-center">
+        <div>
+          <a href="#"><img src="" alt="" srcset=""><h1>MSUKC</h1></a>
+        </div>
+        <div>
+            <ul class="flex-row hidden md:flex space-x-12">
+                <a href=""><li class="">Home</li></a>
+                <a href=""><li class="">About us</li></a>
+                <a href=""><li class="">Services</li></a>
+                <a href=""><li class="">Contact us</li></a>
+            </ul>
+        </div>  
+    </nav>
+    
+  /*Hero Section*/
+    <main class="min-h-screen bg-white text-black flex flex-col md:flex-row mt-5 md:mt-0 text-center md:text-left justify-evenly items-center">
+        <div class="">
+            <h1 class="text-4xl">Welcome to Mindanao State University Karate Club</h1>
+            <p class="text-xl">Become a warrior of the lake</p>
+            <button class="mt-12 py-4 px-6 color-red">Enroll now</button>
+        </div>
+        <div class="h-72 w-72 hidden md:block bg-black">
+            <img src="">Mindanao Image
+        </div>
+    <main>
+</body>
+</html>
